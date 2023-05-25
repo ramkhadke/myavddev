45fca6a994af1d8415bc2b3036ffb7d1683dc09d
ramkhadke <127391510+ramkhadke@users.noreply.github.com>
2023-05-25 17:34:41 +0530

    Create calc.groovy

---
+++ vars/calc.groovy
@@ -0,0 +1,16 @@
+def sum (a,b)
+{
+  echo "the sum of $ {a} and {b} is $ {a+b}"
+}
+def mul (a,b)
+{
+  echo "the mul of $ {a} and {b} is $ {a+b}"
+}
+def subt (a,b)
+{
+  echo "the subt of $ {a} and {b} is $ {a+b}"
+}
+def div (a,b)
+{
+  echo "the div of $ {a} and {b} is $ {a+b}"
+}
